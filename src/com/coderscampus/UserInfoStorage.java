5c7fd1ba7bb8414e7d374e85594d9d8baff73add
Ruth Siguiente <rsiguien@yahoo.com>
2023-07-18 12:11:53 -0400

    second commit on assignment - it is already running just need some cleaning

--- src/com/coderscampus/UserInfoStorage.java
+++ src/com/coderscampus/UserInfoStorage.java
@@ -1,5 +1,51 @@
 package com.coderscampus;
 
-public class UserInfoStorage {
+import java.io.BufferedReader;
+import java.io.FileReader;
 
+public class UserInfoStorage {
+	
+	String username;
+	String password;
+	String name;
+	
+	UserInfoStorage (String username, String password){
+		this.username = username;
+		this.password = password;
+	
+	}
+	
+	public Boolean validateUser() throws Exception {
+		
+		BufferedReader fileReader = null;
+		Boolean flag = false;
+		try { 
+			fileReader = new BufferedReader(new FileReader("data.txt"));
+			
+			String line = "";
+			while ((line = fileReader.readLine()) != null) {
+				//System.out.println(line);
+				String[] strArr = line.split(",");
+				String username = strArr[0];
+				String password = strArr[1];
+		
+				
+				if(username.equalsIgnoreCase(this.username) && password.equals(this.password)) {
+					flag = true;
+					this.name = strArr[2];
+				}
+				
+			}
+			
+		} catch(NullPointerException e) {
+			System.out.println(e.getMessage());
+		}
+		finally {
+			fileReader.close();
+		}
+		return flag;
+	}
+	public String getName() {
+		return name;
+	}
 }
--- src/com/coderscampus/UserValidation.java
+++ src/com/coderscampus/UserValidation.java
@@ -1,10 +1,45 @@
 package com.coderscampus;
+import java.util.Scanner;
 
 public class UserValidation {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception{
+		
+		Boolean isMatch = false;
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		int attempts = 0;
+		while(attempts < 5) {
+		System.out.println("Username: ");
+		String username = scanner.nextLine();
+		System.out.println("Password: ");
+		String password = scanner.nextLine();
+		
+		
+		UserInfoStorage uis = new UserInfoStorage(username, password);
+		isMatch = uis.validateUser();
+		
+		if(attempts == 4) {
+			break;
+		}
+		
+		if(isMatch) {
+			System.out.println("User accepted");
+			System.out.println("Welcome " + uis.getName());
+			break;
+		}
+		else {
+			System.out.println("Invalid login, please try again.");
+		}
+		attempts++;
+		
+		}
+		
+		if(attempts == 4) {
+			System.out.println("Too many failed login attempts, you are now locked out.");
+		}
+		scanner.close();
 	}
 
 }

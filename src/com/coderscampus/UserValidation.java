cd2882d345c49bce7ce8a0cf2b66b7bc930138da
Ruth Siguiente <rsiguien@yahoo.com>
2023-07-31 16:10:25 -0400

    corrected loop procedure to satisfy the assignment requirement

--- src/com/coderscampus/UserValidation.java
+++ src/com/coderscampus/UserValidation.java
@@ -18,20 +18,30 @@ public class UserValidation {
 			UserInfoStorage userInfo = new UserInfoStorage(username, password);
 			isMatch = userInfo.validateUser();
 			
+			if(attempts == 4) {
+				break;
+			}
 			if(isMatch) {
-				System.out.println("User accepted");
 				System.out.println("Welcome " + userInfo.getName());
 				break;	
 			} else {
 				System.out.println("Invalid login, please try again.");
 			}
-			if(attempts == 4) {
-				System.out.println("Too many failed login attempts, you are now locked out.");
-				break;
-			}
-			
 		attempts++;
 		}
-		scanner.close();
+		if(attempts == 4) {
+			System.out.println("Too many failed login attempts, you are now locked out.");
 		}
+		scanner.close();
 	}
+}
+ 
+
+
+
+
+
+
+
+
+

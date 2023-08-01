8ec03b47d3409bd2ee49d61bffa2986f170f0980
Ruth Siguiente <rsiguien@yahoo.com>
2023-07-31 23:07:56 -0400

    removed unnecessary loop in the code

--- src/com/coderscampus/UserValidation.java
+++ src/com/coderscampus/UserValidation.java
@@ -19,18 +19,16 @@ public class UserValidation {
 			isMatch = userInfo.validateUser();
 			
 			if(attempts == 4) {
+				System.out.println("Too many failed login attempts, you are now locked out.");
 				break;
 			}
 			if(isMatch) {
-				System.out.println("Welcome " + userInfo.getName());
+				System.out.println("Welcome: " + userInfo.getName());
 				break;	
 			} else {
 				System.out.println("Invalid login, please try again.");
 			}
 		attempts++;
-		}
-		if(attempts == 4) {
-			System.out.println("Too many failed login attempts, you are now locked out.");
 		}
 		scanner.close();
 	}

3582d711f5b73592988b4fde4d775111f1053d7a
Ruth Siguiente <rsiguien@yahoo.com>
2023-08-11 04:51:26 -0400

    updated code after one on one mentoring with Ogulcan

--- src/com/coderscampus/UserPojo.java
+++ src/com/coderscampus/UserPojo.java
@@ -1,45 +1,50 @@
 package com.coderscampus;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
 public class UserPojo {
 	String username;
 	String password;
 	String name;
 	
-	UserPojo (String username, String password){
+	UserPojo (String username, String password, String name) {
 		this.username = username;
 		this.password = password;
+		this.name = name;
 	}
 	
-	public Boolean userService() throws Exception {
-		BufferedReader fileReader = null;
-		Boolean flag = false;
-		try { 
-			fileReader = new BufferedReader(new FileReader("data.txt"));
-			String line = "";
-			while ((line = fileReader.readLine()) != null) {
-				String[] stringArray = line.split(",");
-				String username = stringArray[0];
-				String password = stringArray[1];
-		
-				if(username.equalsIgnoreCase(this.username) && password.equals(this.password)) {
-					flag = true;
-					this.name = stringArray[2];
-					break;
-				}	
-			}
-		} catch (NullPointerException e) {
-			System.out.println(e.getMessage());
-		}
-		finally {
-			fileReader.close();
-		}
-		return flag;
-	}
+	
 	public String getName() {
 		return name;
 	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	@Override
+	public String toString() {
+		return "UserPojo [username=" + username + ", password=" + password + ", name=" + name + "]";
+	}
+	
 }
 				
